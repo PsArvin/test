83180eb3c41e515655165c13ea704328fb33f0c8
LIWENHUA <756883624@qq.com>
2015-03-18 19:12:58 +0800

    hehehe

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		test t=new test();
-		System.out.println(t.Test()+"hehe");
+		System.out.println(t.Test()+"hehehehe");
+		System.out.println(t.Test()+"hehehehe");
 		t.toMsg();
 	}
 }
