948240f5dda1c965e320afb5961815f55aede5d3
LIWENHUA <LIWENHUA@CDUT5818.CDUT5818.COM>
2015-03-18 18:54:13 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import tool.test;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		test t=new test();
+		System.out.println(t.Test());
+		t.toMsg();
+	}
+}
---
+++ src/tool/test.java
@@ -0,0 +1,12 @@
+package tool;
+
+public class test {
+
+	public String Test(){
+		return "hehe";
+	}
+	
+	public void toMsg()	{
+		System.out.println("这是一个开始...");
+	}
+}
